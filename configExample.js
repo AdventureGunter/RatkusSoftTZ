27ae098cca751db365091c20748590ee692d3e14
AdventureGunter <vvovvrulit@gmail.com>
2017-07-11 11:52:27 +0300

    added configExample

---
+++ configExample.js
@@ -0,0 +1,12 @@
+/**
+ * Created by User on 07.07.2017.
+ */
+module.exports = {
+    db: {
+        url : 'Your mongodb url, example', //example - 'mongodb://127.0.0.1:27017/ratkusSoftTz'
+        admin: {
+            username: '...', //put admin username here
+            password: '...'  //put admin password here
+        }
+    }
+};
